1d9ba4c914599af260c14e19a445a7f6ef78af48
github username <github email address>
2020-02-18 23:12:31 -0500

    updated functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-let time = 5;
-let score = 0;
-let isPlaying;  
+window.addEventListener('load', startGame);
 
-// DOM Elements 
-const wordInput = document.getElementById("word-input")
-const currentWord = document.getElementById("current-word")
-const scoreDisplay = document.getElementById("score")
-const timeDisplay = document.getElementById("time")
-const message = document.getElementById("message")
-const seconds = document.getElementById("seconds")
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 1
+};
+
+// To change level
+const currentLevel = levels.medium;
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.getElementById("word-input");
+const currentWord = document.getElementById("current-word");
+const scoreDisplay = document.getElementById("score");
+const timeDisplay = document.getElementById("time");
+const message = document.getElementById("message");
+const seconds = document.getElementById("seconds");
 
 const words =[
 'line', 'chivalrous', 'veil', 'near', 'accept', 'unbecoming', 'admit', 'knowing', 'young', 'wave', 
@@ -19,4 +30,67 @@ const words =[
 'move', 'elbow', 'tongue', 'idea', 'free', 'berry','pathetic', 'interfere', 'broken', 'salt', 
 'yielding', 'garage', 'animated', 'church', 'calculate','impossible', 'comparison', 'kaput', 
 'even', 'describe', 'fancy', 'approval', 'solid', 'recondite'
-]
+];
+
+function startGame() {
+  seconds.innerText = currentLevel;
+
+  showWords(words);
+
+  wordInput.addEventListener('input', startMatch);
+
+  setInterval(countdown, 1000);
+  setInterval(checkStatus, 50);
+}
+
+// Start match
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWords(words);
+    wordInput.value = '';
+    score++;
+  }
+
+  if (score === -1) {
+    scoreDisplay.innerText = 0;
+  } else {
+    scoreDisplay.innerText = score;
+  }
+}
+
+// Matches input with currentWord
+function matchWords() {
+  if (wordInput.value === currentWord.innerText) {
+    message.innerText = 'Correct!!!';
+    return true;
+  } else {
+    message.innerText = '';
+    return false;
+  }
+}
+
+// Show random word
+function showWords(words) {
+  const randIndex = Math.floor(Math.random() * words.length);
+  currentWord.innerText = words[randIndex];
+}
+
+// Countdown timer
+function countdown() {
+  if (time > 0) {
+    time--;
+  } else if (time === 0) {
+    isPlaying = false;
+  }
+  timeDisplay.innerText = time;
+}
+
+// Check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerText = 'Game Over!!!';
+    score = -1;
+  }
+}
